aa5bcfdf4c877c30034fa5b7d7ccb64a5ef414b8
Philipp Schoneville <philipp@schoneville.com>
2026-01-08 11:20:36 +0100

    build: Add some building stuff

---
+++ scripts/publish_frontend.sh
@@ -0,0 +1,10 @@
+mkdir -p docs/results/
+
+# Export the notebook
+uvx marimo export html-wasm --mode run --no-show-code \
+  frontend/magical_athlete_analysis.py \
+  -o docs
+
+# Copy supporting files
+cp frontend/magical_athlete_analysis.css docs/
+cp -r results/*.parquet docs/results/
